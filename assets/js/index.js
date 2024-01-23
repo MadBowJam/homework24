1cbeaa412527129d295c0c55104ceecaec66e0ea
MadBowJam <madbowjam@gmail.com>
2024-01-23 22:24:28 +0200

    release

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+import SwipeCarousel from './swipe-carousel.js';
+
+let carousel = new SwipeCarousel({
+  containerID: '#carousel',
+  interval: 3000
+  // isPlaying: false
+});
+
+carousel.init();
---
+++ assets/js/swipe-carousel.js
@@ -0,0 +1,36 @@
+import Carousel from './carousel.js';
+
+class SwipeCarousel extends Carousel {
+  constructor(...args) {
+    super(...args);
+    this.slidesContainer = this.slideItems[0].parentElement;
+  }
+
+  /* private, _initListeners function */
+  _initListeners() {
+    super._initListeners();
+    this.container.addEventListener('touchstart', this._swipeStart.bind(this));
+    this.slidesContainer.addEventListener('mousedown', this._swipeStart.bind(this));
+    this.container.addEventListener('touchend', this._swipeEnd.bind(this));
+    this.slidesContainer.addEventListener('mouseup', this._swipeEnd.bind(this));
+  }
+
+  /* private, _swipeStart function */
+  _swipeStart(e) {
+    this.startPosX = e instanceof MouseEvent
+        ? e.pageX
+        : e.changedTouches[0].pageX;
+  }
+
+  /* private, _swipeEnd function */
+  _swipeEnd(e) {
+    this.endPosX = e instanceof MouseEvent
+        ? e.pageX
+        : e.changedTouches[0].pageX;
+
+    if (this.endPosX - this.startPosX > 100) this.prev();
+    if (this.endPosX - this.startPosX < -100) this.next();
+  }
+}
+
+export default SwipeCarousel;
